ccf96fa8b46a85b5a49a55d7b564d278a9962a60
vishal.chhabra <vishal.chhabra@tothenew.com>
2019-12-15 18:18:53 +0530

    first commit

---
+++ pages/homepage.js
@@ -0,0 +1,28 @@
+let homepage = function() {
+    let Input1 =  element(by.model('first'));
+    let Input2 = element(by.model('second'));
+    let btn = element(by.id('gobutton'));
+    let result = element(by.css('.ng-binding'));
+
+    this.getURL = function(url) {
+        browser.get(url);
+    };
+    this.firstInput = function(input){
+        Input1.sendKeys(input);
+    };
+    this.secondInput = function(input){
+        Input2.sendKeys(input);
+    };
+    this.button = function(){
+        btn.click();
+    };
+    this.checkResult = function(res){
+        expect(result.getText()).toEqual(res);
+    };
+    this.browserSleep = function(ms){
+        browser.sleep(ms);
+    };
+};
+
+module.exports = new homepage();
+
---
+++ tests/calculator.spec.js
@@ -0,0 +1,37 @@
+var homepage = require('../pages/homepage');
+
+describe("calculator test suite", function() {
+
+    it("Addition Test", function() {
+        // browser.get('http://juliemr.github.io/protractor-demo/');
+        homepage.getURL('http://juliemr.github.io/protractor-demo/');
+        // element(by.model('first')).sendKeys(1);
+        homepage.firstInput(1);
+        // element(by.model('second')).sendKeys(9);
+        homepage.secondInput(9);
+        // element(by.id('gobutton')).click();
+        homepage.button();
+        // let result = element(by.css('.ng-binding'));
+        // expect(result.getText()).toEqual('10');
+        homepage.checkResult('10');
+        // browser.sleep(2000);
+        homepage.browserSleep(2000);
+    });
+
+    it("Subtraction Test", function() {
+
+    });
+
+    it("Multiplication Test", function() {
+
+    });
+
+    it("Division Test", function() {
+
+    });
+
+    it("Modulus Test", function() {
+
+    });
+
+});
