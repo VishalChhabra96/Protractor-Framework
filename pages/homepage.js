82a7950791746de6540acc9c61a07c5aa909c927
vishal.chhabra <vishal.chhabra@tothenew.com>
2019-12-16 15:02:26 +0530

    updating result css selection

--- pages/homepage.js
+++ pages/homepage.js
@@ -2,7 +2,7 @@ let homepage = function() {
     let Input1 =  element(by.model('first'));
     let Input2 = element(by.model('second'));
     let btn = element(by.id('gobutton'));
-    let result = element(by.css('.ng-binding'));
+    let result = element(by.css('h2.ng-binding'));
 
     this.getURL = function(url) {
         browser.get(url);
